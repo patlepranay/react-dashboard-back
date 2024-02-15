29268d0bdc3a4d399d723148e67ce35e42ef98b1
Pranay Patle <pranayhpatle@gmail.com>
2024-02-16 00:14:52 +0530

    updated util and cors

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://react-live-dashboard.netlify.app",
   },
   transports: ["websocket", "polling"],
 });
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,6 @@
 import { generateSlug } from "random-word-slugs";
 
-export const generateRandomData = (length:number,companyArray?:string[]) => {
+export const generateRandomData = (length: number, companyArray?: string[]) => {
   // Function to generate random data for a company
   function generateRandomCompanyData() {
     const revenue = Math.floor(Math.random() * (1000000 - 100000) + 100000); // Random revenue between 100,000 and 1,000,000
@@ -11,10 +11,12 @@ export const generateRandomData = (length:number,companyArray?:string[]) => {
     const profitPercentage = ((profit / revenue) * 100).toFixed(2);
 
     const lastTenYearData = Array.from({ length: 10 }, (_, index) => ({
-      year: new Date().getFullYear() - index,
-      totalRevenueValue: Math.floor(Math.random() * (100000 - 10000) + 10000),
-      profitValue: Math.floor(Math.random() * (10000 - 1000) + 1000),
-      employeeCount:Math.floor(Math.random() * (1000 - 100) + 10),
+      year: Number(new Date().getFullYear() - index),
+      totalRevenueValue: Number(
+        Math.floor(Math.random() * (100000 - 10000) + 10000)
+      ),
+      profitValue: Number(Math.floor(Math.random() * (10000 - 1000) + 1000)),
+      employeeCount: Number(Math.floor(Math.random() * (1000 - 100) + 10)),
     }));
 
     return {
@@ -28,7 +30,9 @@ export const generateRandomData = (length:number,companyArray?:string[]) => {
 
   // Generate random data for 5 companies
   const companies = Array.from({ length: 5 }, (_, index) => ({
-    name: companyArray ? companyArray[index] :generateSlug(2, { format: "title" }),
+    name: companyArray
+      ? companyArray[index]
+      : generateSlug(2, { format: "title" }),
     ...generateRandomCompanyData(),
   }));
 
